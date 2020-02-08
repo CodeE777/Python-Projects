a547164534f6e1ae38fa75137683584ff32240df
ajpgross <ajpgross2@gmail.com>
2020-02-08 13:53:02 -0700

    Figured out how to print close vals with dates

--- MarketTrendAnalyis.py
+++ MarketTrendAnalyis.py
@@ -4,10 +4,20 @@ from Envrionment import Environment
 
 time_series_weekly = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=.INX&apikey={Environment.apikey}")
 
-Parsed_time_series = json.loads(time_series_weekly.content)
+parsed_time_series = json.loads(time_series_weekly.content)['Time Series (Daily)']
 
-print(Parsed_time_series)
+for date in parsed_time_series:
+    print(date)
+    print(parsed_time_series[date]['4. close'])
 
+# print(test)
 
+# r1 = { 'fname' : 'Kent', 'lname' : 'Johnson' }
+# r2 = { 'fname' : 'Negroup', 'lname' : None }
+# data = [ r1, r2 ]
 
+# # Suppose I want to find everyone whose first name is 'Kent'. This is easy to do with a list comprehension:
 
+# test = [ r for r in data if r['fname'] == 'Kent' ]
+
+# print(test)
